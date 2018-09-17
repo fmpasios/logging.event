79e16cba612e259876794c28ca520bf831887068
fmpasios <fotios.basios@gmail.com>
2018-09-17 10:55:35 +0300

    Creating cassandra's schema and table when connector is created

--- src/main/java/org/pollfish/DatabaseConnector/CassandraConnector.java
+++ src/main/java/org/pollfish/DatabaseConnector/CassandraConnector.java
@@ -16,6 +16,26 @@ public class CassandraConnector {
 
     final static int DB_SERVER_PORT=9042;
 
+    final static String CREATING_KEY_SPACE="CREATE KEYSPACE IF NOT EXISTS logging" +
+            "    WITH replication = {'class': 'SimpleStrategy'," +
+            "                        'replication_factor' : 1};";
+
+
+    final static String CREATING_LOG_MESSAGE_TYPE="CREATE TYPE IF NOT EXISTS logging.log_message (" +
+            "  level text," +
+            "  log_time timestamp," +
+            "  message text" +
+            ");";
+
+    final static String CREATING_EVENT_TABLE="CREATE TABLE IF NOT EXISTS logging.event (" +
+            "  id UUID PRIMARY KEY," +
+            "  version smallint," +
+            "  event_time timestamp," +
+            "  device_id text," +
+            "  ip_address text," +
+            "  message FROZEN<log_message>" +
+            " );";
+
 
     public static void createDatabaseConnector()
     {
@@ -23,6 +43,15 @@ public class CassandraConnector {
         cluster =  Cluster.builder().withPort(DB_SERVER_PORT)
                 .addContactPoints(DB_SERVER_IP)
                 .build();
+        Session session=cluster.connect();
+
+        session.execute(CREATING_KEY_SPACE);
+        session.execute(CREATING_LOG_MESSAGE_TYPE);
+        session.execute(CREATING_EVENT_TABLE);
+
+
+        session.close();
+
 
     }
 
